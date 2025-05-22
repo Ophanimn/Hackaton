471d35450a46242425d2456e6deae0ed91924978
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-05-22 11:13:56 +0200

    Update bdd.php

---
+++ bdd.php
@@ -0,0 +1,31 @@
+<?php
+class BDD {
+
+	private $mysqli;
+	
+
+	public function __construct() {
+		$this -> mysqli = false;
+	}
+
+	/* Connexion à la base de données */
+	public function connexion() {
+		mysqli_report(MYSQLI_REPORT_OFF);
+		
+		$this -> mysqli = new mysqli('172.16.119.3', 'operateur', 'operateur', 'Quizz');
+
+		if($this -> mysqli -> connect_errno != 0) {
+			return false;
+		}
+		else return true;
+	}
+	
+	
+	/* Déconnexion à la base de données */
+	public function deconnexion() {
+		if($this -> mysqli -> connect_errno != 0) {
+			$this -> mysqli -> close();
+		}
+	}
+}
+?>
