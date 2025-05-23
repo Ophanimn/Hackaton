0f35022ac4632cadf0d4aa5d73fc7f1b4ee27fd4
MaximeHenault <maxime.henault@lyceefulbert.fr>
2025-05-23 10:38:08 +0200

    appmvc finit normalement

--- bdd.php
+++ bdd.php
@@ -109,7 +109,7 @@ class BDD {
 		$lettre = null;	//Servira a stocker les lettres
 
 		/* On crée la requete SQL et on lie les paramètres */
-		$requete = $this -> mysqli-> prepare("SELECT nom, prenom, pays, ville, type_voie, code_postal, libelle, Lettre.status FROM Enfant INNER JOIN Adresse ON Enfant.id_Enfant = Adresse.id_Adresse INNER JOIN Lettre ON Lettre.id_Enfant = Enfant.id_Enfant INNER JOIN livre ON Lettre.id_Lettre = livre.id_Lettre INNER JOIN Type_cadeau ON livre.id_TypeCadeau = Type_cadeau.id_TypeCadeau GROUP BY nom");
+		$requete = $this -> mysqli-> prepare("SELECT Enfant.nom, Enfant.prenom, GROUP_CONCAT(Type_cadeau.libelle SEPARATOR ', ') AS cadeaux FROM Enfant INNER JOIN Lettre ON Lettre.id_Enfant = Enfant.id_Enfant INNER JOIN livre ON Lettre.id_Lettre = livre.id_Lettre INNER JOIN Type_cadeau ON livre.id_TypeCadeau = Type_cadeau.id_TypeCadeau GROUP BY Enfant.nom, Enfant.prenom ORDER BY Enfant.nom, Enfant.prenom");
 		if (!$requete) {
     		return $lettre;
 		}
@@ -134,7 +134,7 @@ class BDD {
 		$lettre = null;	//Servira a stocker les lettres
 
 		/* On crée la requete SQL et on lie les paramètres */
-		$requete = $this -> mysqli-> prepare("SELECT libelle, stock FROM TypeCadeau");
+		$requete = $this -> mysqli-> prepare("SELECT libelle, stock FROM Type_cadeau");
 		if (!$requete) {
     		return $lettre;
 		}
@@ -179,7 +179,54 @@ class BDD {
 		return $lettre;
 	}
 
-	
-	
+	public function getAtelier()
+	{
+		$lettre = null;	//Servira a stocker les lettres
+
+		/* On crée la requete SQL et on lie les paramètres */
+		$requete = $this -> mysqli-> prepare("SELECT Atelier.secteur, Atelier.adresse, COUNT(Lutin.id_Lutin) AS nb_lutins, GROUP_CONCAT(CONCAT(Lutin.nom, ' ', Lutin.prenom) SEPARATOR ', ') AS noms_lutins FROM Atelier INNER JOIN Lutin ON Lutin.id_Atelier = Atelier.id_Atelier GROUP BY Atelier.id_Atelier, Atelier.secteur, Atelier.adresse ORDER BY Atelier.secteur;");
+		if (!$requete) {
+    		return $lettre;
+		}
+		
+		/* On execute la requete et on récupère le résultat */
+		$requete -> execute();
+		$resultat = $requete -> get_result();
+		
+		/* On libère la requête */
+		$requete -> close();
+			
+		/* On parcours les résultats pour les stocker */
+		if($enregistrement = $resultat -> fetch_object()) {
+				$lettre = $enregistrement;
+		}	
+		
+		return $lettre;
+	}
+
+	public function getLivraison()
+	{
+		$lettre = null;	//Servira a stocker les lettres
+
+		/* On crée la requete SQL et on lie les paramètres */
+		$requete = $this -> mysqli-> prepare("SELECT Livraison.datelivraison, Lettre.status, GROUP_CONCAT(CONCAT(Lutin.nom, ' ', Lutin.prenom) SEPARATOR ', ') AS lutins, GROUP_CONCAT(Type_cadeau.libelle SEPARATOR ', ') AS cadeaux FROM Livraison INNER JOIN gere ON Livraison.id_Livraison = gere.id_Livraison INNER JOIN Lutin ON gere.id_Lutin = Lutin.id_Lutin INNER JOIN livre ON Livraison.id_Livraison = livre.id_Livraison INNER JOIN Type_cadeau ON livre.id_TypeCadeau = Type_cadeau.id_TypeCadeau INNER JOIN Lettre ON livre.id_Lettre = Lettre.id_Lettre INNER JOIN Enfant ON Lettre.id_Enfant = Enfant.id_Enfant GROUP BY Livraison.datelivraison, Lettre.status ORDER BY Livraison.datelivraison;");
+		if (!$requete) {
+    		return $lettre;
+		}
+		
+		/* On execute la requete et on récupère le résultat */
+		$requete -> execute();
+		$resultat = $requete -> get_result();
+		
+		/* On libère la requête */
+		$requete -> close();
+			
+		/* On parcours les résultats pour les stocker */
+		if($enregistrement = $resultat -> fetch_object()) {
+				$lettre = $enregistrement;
+		}	
+		
+		return $lettre;
+	}
 }
 ?>
